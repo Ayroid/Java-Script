4844418d78d14f7a6953fda2ee66879e8d8ed70a
root <root@Ayroid>
2023-02-25 17:52:00 +0530

    .

--- Chapter3/Practice/Question1.js
+++ Chapter3/Practice/Question1.js
@@ -2,8 +2,9 @@ let marks = {
     Harry : 98,
     Rohan : 70,
     Aakash : 7
-}
+};
 
+//for loop
 for(let i=0;i<Object.keys(marks).length;i++){
     console.log("Marks of "+Object.keys(marks)[i]+" are "+marks[Object.keys(marks)[i]])
 }
---
+++ Chapter3/Practice/Question2.js
@@ -0,0 +1,10 @@
+let marks = {
+    Harry : 98,
+    Rohan : 70,
+    Aakash : 7
+};
+
+//for in loop
+for (i in marks){
+    console.log("Marks of "+ i +" are "+ marks[i]);
+}
