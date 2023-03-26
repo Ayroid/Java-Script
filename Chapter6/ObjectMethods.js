0389785de1c3a24273b83ee19c6f6a1b0222ec5c
Ayroid <ayushsk0000@gmail.com>
2023-03-26 07:52:13 +0530

    chapter 6 complete

---
+++ Chapter6/ObjectMethods.js
@@ -0,0 +1,11 @@
+console.clear(); //Clears the console
+
+console.log("Hello!");
+
+console.assert(5<2); // Used to assert condition
+
+console.warn("HAHAHA"); // To show any warning
+
+console.log(console);
+
+console.log(document);
